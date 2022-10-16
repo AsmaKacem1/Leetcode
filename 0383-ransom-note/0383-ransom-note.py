f25ac40e62f2f8b563fafb8bb2c0410d39291279
Asma Kacem <asma.kacem217@gmail.com>
2022-10-16 10:29:40 +0100

    Time: 81 ms (66.14%), Space: 14.2 MB (18.03%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dic={}
+        if (len(ransomNote)==len(magazine)==1):
+            return ransomNote==magazine
+        elif (len(ransomNote)>len(magazine)):
+                return False
+        else:
+            for i in ransomNote:
+                if i in magazine:
+                    magazine=magazine.replace(i,'',1)
+                else:
+                    return False
+            return True
