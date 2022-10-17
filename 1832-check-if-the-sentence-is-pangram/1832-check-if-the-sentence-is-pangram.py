9a1de8fb1984bc95e3fbd600a2d309098cbda6f7
Asma Kacem <asma.kacem217@gmail.com>
2022-10-17 10:49:08 +0100

    Time: 59 ms (43.23%), Space: 13.9 MB (54.73%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,5 +1,7 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
+        if(len(sentence)<26):
+            return False
         my_set=set()
         for i in range(len(sentence)):
             my_set.add(sentence[i])
