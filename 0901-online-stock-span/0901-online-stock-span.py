07eae47ee6b552b7384932a864ab6f1281a79e32
Asma Kacem <asma.kacem217@gmail.com>
2022-11-10 00:15:42 +0100

    Time: 812 ms (68.11%), Space: 19.6 MB (14.93%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,33 @@
+class StockSpanner:
+
+    
+            
+            
+    def __init__(self):
+        self.st=[]
+        self.lst=[]
+        
+
+    def next(self, price: int) -> int:
+        if self.st==[]:
+            self.st.append(price)
+            self.lst.append(1)
+            return 1
+        ct=1
+        while self.st:
+            if self.st[-1]<=price:
+                ct+=self.lst.pop()
+                self.st.pop()
+            else:
+                break
+        self.st.append(price)
+        self.lst.append(ct)
+        return ct
+        
+            
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
